544e21f2d19f20a7a4cfff96775032dc3ee225a9
Alex-Cartwright <cartwrighta96@gmail.com>
2024-06-22 11:37:01 +0100

    working dropdown

--- src/features/booking/api/fetchLocations.ts
+++ src/features/booking/api/fetchLocations.ts
@@ -1,10 +1,19 @@
+export type Location = {
+  name: string
+  url: string
+}
+
 const locations = [
   {
     name: 'Islington',
     url: ''
+  },
+  {
+    name: 'Lea Valley',
+    url: ''
   }
 ]
 
-export const fetchLocations = () : object[] => {
+export const fetchLocations = () : Location[] => {
   return locations
 }
--- src/features/booking/routes/Booking.tsx
+++ src/features/booking/routes/Booking.tsx
@@ -1,14 +1,34 @@
 import { DateCalendar } from "@mui/x-date-pickers"
+import { fetchLocations } from "../api/fetchLocations"
+import { Box, FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import { useState } from "react";
 
 export const Booking = () => {
 
+  const locations = fetchLocations();
+
+  const [location, setLocation] = useState('');
+
   return (
-    <div>
+    <Box>
       <h2>Book A Court</h2>
+      <FormControl fullWidth>
+        <InputLabel id="booking-location-label">Location</InputLabel>
+        <Select
+          labelId="booking-location-label"
+          id="booking-location"
+          label="Location"
+          onChange={(e) => setLocation(e.target.value as string)}
+        >
+          {locations.map((location, index) => (
+            <MenuItem key={index} value={location.name}>{location.name}</MenuItem>
+          ))}
+        </Select>
+      </FormControl>
       <h3>Location</h3>
       <DateCalendar />
       <h3>Times</h3>
       ....
-    </div>
+    </Box>
   )
 }
