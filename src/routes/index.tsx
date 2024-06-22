388a99796ee6bfa67de55ba7037c28e0d8ea988f
Alex-Cartwright <cartwrighta96@gmail.com>
2024-06-22 10:52:07 +0100

    add home landing page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,23 +1,15 @@
 import { Outlet, useRoutes } from "react-router-dom"
 import { Scheduler } from "../features/scheduler"
-
-const App = () => {
-  return (
-    <div>
-      <h1>Tennis Bot</h1>
-      <Outlet />
-    </div>
-  )
-
-}
+import { Home } from "../features/home"
 
 export const AppRoutes = () => {
 
   const routes = [
     {
-      path: '/app',
-      element: <App />,
+      path: '/',
+      element: <Outlet />,
       children: [
+        { index: true, element: <Home /> },
         { path: 'scheduler', element: <Scheduler /> },
       ]
     }
